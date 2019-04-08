a1b028d40be34a9b8ce84bc5f34ee469745bf630
Andreas Lagoni <andreastlagoni@gmail.com>
2019-04-08 11:38:21 +0200

    frontpage on employee

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function redirectAnsat() {
   let modal = document.getElementById("redirectAnsat");
   modal.classList.add("redirect");
   setTimeout(function() {
-    window.location.replace("#");
-    window.location.reload();
+    window.location.replace("danishversion/frontpage.html");
   }, 3000);
 }
 function redirectLeder() {
