1c2308bec991bce0298152d9a3e10ebfb8c6896b
AndreasLagoni <46136193+AndreasLagoni@users.noreply.github.com>
2019-04-09 19:14:47 +0200

    changing files and paths

---
+++ englishversion/js/englishjs.js
@@ -0,0 +1,5 @@
+var messageModal = document.getElementById("messageModal");
+function openModal() {
+  messageModal.classList.toggle("open");
+  document.getElementById("newMessage").style.display = "none";
+}
