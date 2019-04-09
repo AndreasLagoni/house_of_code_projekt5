35cc41d0f67573da37f235e9a56fa6af9af24b4e
AndreasLagoni <46136193+AndreasLagoni@users.noreply.github.com>
2019-04-09 16:17:33 +0200

    added "all messages"

--- danishversion/danishjs.js
+++ danishversion/danishjs.js
@@ -1,8 +1,4 @@
 var messageModal = document.getElementById("messageModal");
 function openModal() {
-  if (messageModal.classList.contains("open")) {
-    messageModal.classList.remove("open");
-  } else {
-    messageModal.classList.add("open");
-  }
+  messageModal.classList.toggle("open");
 }
