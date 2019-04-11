3484504d009cc2c5bfcab9ddbad8abd802e5a2d0
AndreasLagoni <46136193+AndreasLagoni@users.noreply.github.com>
2019-04-11 23:48:06 +0200

    animation på medarbejdere

---
+++ danishversion/js/medarbejderload.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  let maincontent = document.getElementById("medarbejderMainContent");
+  maincontent.style.opacity = "100";
+  maincontent.style.left = "0";
+  let historyContent = document.getElementById("medarbejderHistory");
+  historyContent.style.opacity = "100";
+  historyContent.style.left = "0";
+  let myData = document.getElementById("myData");
+  myData.style.opacity = "100";
+  myData.style.top = "0";
+  let employeeRoles = document.getElementById("employeeRoles");
+  employeeRoles.style.opacity = "100";
+  employeeRoles.style.top = "0";
+};
