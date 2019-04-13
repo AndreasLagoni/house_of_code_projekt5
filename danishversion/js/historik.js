485d2e5d7a6cca1e74fa5a47a9537058901adf7c
AndreasLagoni <46136193+AndreasLagoni@users.noreply.github.com>
2019-04-13 17:21:18 +0200

    historik done MED

---
+++ danishversion/js/historik.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+  let historyContent = document.getElementById("medarbejderHistory");
+  historyContent.style.opacity = "100";
+  historyContent.style.left = "50px";
+  let myData = document.getElementById("myData");
+  myData.style.opacity = "100";
+  myData.style.top = "0";
+  let employeeRoles = document.getElementById("employeeRoles");
+  employeeRoles.style.opacity = "100";
+  employeeRoles.style.top = "0";
+};
+var messageModal = document.getElementById("messageModal");
+function openModal() {
+  messageModal.classList.toggle("open");
+  document.getElementById("newMessage").style.display = "none";
+}
