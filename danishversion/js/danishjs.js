37e1b11d08a0dc93c53f9399f5ae342fc966cb83
Andreas Lagoni <andreastlagoni@gmail.com>
2019-04-16 14:28:51 +0200

    hover on standardbtn

--- danishversion/js/danishjs.js
+++ danishversion/js/danishjs.js
@@ -3,3 +3,15 @@ function openModal() {
   messageModal.classList.toggle("open");
   document.getElementById("newMessage").style.display = "none";
 }
+
+var evaluereLink = document.getElementById("green");
+evaluereLink.onclick = function() {
+  let mainWrapper = document.getElementById("mainWrapper");
+  let bg = document.getElementById("bgimg");
+  bg.style.opacity = "0";
+  mainWrapper.style.opacity = "0";
+  mainWrapper.style.visibility = "hidden";
+  setTimeout(function() {
+    window.location.href = "evaluere/DAevaluerMED.html";
+  }, 500);
+};
