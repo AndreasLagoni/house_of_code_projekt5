8f42d3b653adb5577c2bfc6b8a4b1810b2b542ba
Andreas Lagoni <andreastlagoni@gmail.com>
2019-04-17 15:09:03 +0200

    added account modal on all pages

--- danishversion/js/modal.js
+++ danishversion/js/modal.js
@@ -1,5 +1,19 @@
 var messageModal = document.getElementById("messageModal");
+var accountModal = document.getElementById("accountModal");
 function openModal() {
-  messageModal.classList.toggle("open");
+  if ((accountModal.classList.contains = "open")) {
+    accountModal.classList.remove("open");
+    messageModal.classList.toggle("open");
+  } else {
+    messageModal.classList.toggle("open");
+  }
   document.getElementById("newMessage").style.display = "none";
+}
+function openAccountModal() {
+  if ((messageModal.classList.contains = "open")) {
+    messageModal.classList.remove("open");
+    accountModal.classList.toggle("open");
+  } else {
+    accountModal.classList.toggle("open");
+  }
 }
