d9bd09f73ee9a8c72c0b87c33526c8406b0d63d0
Jeppe Adler <Jeppeadler@gmail.com>
2019-04-23 11:46:41 +0200

    Mellemrum fjernet

--- danishversion/js/evaluering.js
+++ danishversion/js/evaluering.js
@@ -48,13 +48,11 @@ function addGoal() {
   let goalWrapper = document.getElementById("goalWrapper");
   let newDiv = document.createElement("div");
   newDiv.innerHTML = `<div class="goalItemWrapper" id="goalItem">
-  
     <a href="javascript:addGoal()">
       <i class="far fa-plus-square"></i
     ></a>
-    <a 
+    <a
     onclick="this.parentElement.style.display = 'none'"><i class="far fa-minus-square"></i></a>
- 
   <h3>Mål:</h3>
   <input type="text" />
 </div>`;
